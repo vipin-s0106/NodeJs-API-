50f13be03c84c74dc06e75b309473ea1decd1486
vipin-s0106 <vipin.s0106@gmail.com>
2020-08-18 20:08:18 +0530

    CORS middleware updated

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin','http://localhost:3000');
     res.header(
         'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
     if (req.method === 'OPTIONS'){
         res.header(
